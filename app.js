3cca8eb7407b5ec9a305a085c5e80835053c8e26
Vishruth <vishruthvs2003@gmail.com>
2024-02-23 00:25:16 +0530

    Uploading files onto google drive

---
+++ app.js
@@ -0,0 +1,46 @@
+const { google } = require("googleapis");
+const path = require("path");
+const fs = require("fs");
+
+const CLIENT_ID =
+  "241763807689-5v8mum3rdnsprh6d40hied5cihinnugu.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+
+const REFRESH_TOKEN =
+  "1//04NKAcKENeB5pCgYIARAAGAQSNwF-L9IrCf-QPF8VGPrU5zVCXW9OgtR0urRzD8stVGdok-vkrSjvSqhL6QJsFZDaLdLzLkHLgFA";
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const drive = google.drive({
+  version: "v3",
+  auth: oauth2Client,
+});
+
+const filePath = path.join(__dirname, "profilepic.jpg");
+
+async function uploadFile() {
+  try {
+    const response = await drive.files.create({
+      requestBody: {
+        name: "hackerpic.jpg",
+        mimeType: "image/jpg",
+      },
+      media: {
+        mimeType: "image/jpg",
+        body: fs.createReadStream(filePath),
+      },
+    });
+    console.log(response.data);
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+//for uploading files
+uploadFile();
