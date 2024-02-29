46c001e8188a6eee751f63600b683d8190b9638d
Vishruth <vishruthvs2003@gmail.com>
2024-02-29 23:12:15 +0530

    added env files

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const { google } = require("googleapis");
 const path = require("path");
 const fs = require("fs");
+require("dotenv").config();
+// const CLIENT_ID =
+//   "241763807689-5v8mum3rdnsprh6d40hied5cihinnugu.apps.googleusercontent.com";
+// const CLIENT_SECRET = "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";
+// const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
-const CLIENT_ID =
-  "241763807689-5v8mum3rdnsprh6d40hied5cihinnugu.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-
-const REFRESH_TOKEN =
-  "1//04NKAcKENeB5pCgYIARAAGAQSNwF-L9IrCf-QPF8VGPrU5zVCXW9OgtR0urRzD8stVGdok-vkrSjvSqhL6QJsFZDaLdLzLkHLgFA";
-
+// const REFRESH_TOKEN =
+//   "1//04NKAcKENeB5pCgYIARAAGAQSNwF-L9IrCf-QPF8VGPrU5zVCXW9OgtR0urRzD8stVGdok-vkrSjvSqhL6QJsFZDaLdLzLkHLgFA";
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
@@ -23,17 +26,17 @@ const drive = google.drive({
   auth: oauth2Client,
 });
 
-const filePath = path.join(__dirname, "profilepic.jpg");
+const filePath = path.join(__dirname, "CANSAT.pdf");
 
 async function uploadFile() {
   try {
     const response = await drive.files.create({
       requestBody: {
-        name: "hackerpic1.jpg",
-        mimeType: "image/jpg",
+        name: "Cansat.pdf",
+        mimeType: "application/pdf",
       },
       media: {
-        mimeType: "image/jpg",
+        mimeType: "application/pdf",
         body: fs.createReadStream(filePath),
       },
     });
@@ -43,11 +46,11 @@ async function uploadFile() {
   }
 }
 //for uploading files
-uploadFile();
+// uploadFile();
 async function deleteFile() {
   try {
     const response = await drive.files.delete({
-      fileId: "1SHzMbvWcVaeBGNidDFjB5A9ruZeDIumB",
+      fileId: "1ypyQDXs0Qp9KOfpxsGLidBbW19uyH6sj",
     });
     console.log(response.data, response.status);
   } catch (e) {
@@ -57,7 +60,7 @@ async function deleteFile() {
 // deleteFile();
 async function generatePublicUrl() {
   try {
-    const fileId = "1H3dFItfWBquvhJgdj5erwViw997V5WxS";
+    const fileId = "1ypyQDXs0Qp9KOfpxsGLidBbW19uyH6sj";
     await drive.permissions.create({
       fileId: fileId,
       requestBody: {
